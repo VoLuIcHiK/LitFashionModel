82f6acc83a71d87ff43c48d2cef98ecccfd79821
Valueva Anastasiya <valueva200211@gmail.com>
2023-10-31 02:07:13 +0300

    Пытаюсь добавить wandb

--- lesson4/train.py
+++ lesson4/train.py
@@ -3,6 +3,7 @@ from .nn import LitFashionMNIST
 import lightning as L
 #import wandb
 from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
 LEARNING_RATE = 0.0001
 BATCH_SIZE = 64
 EPOCHS = 10
@@ -21,15 +22,10 @@ CONFIG = dict(
 def train(PATH):
     #run = wandb.init(project="fashion_mnist_lit", reinit=True, config=CONFIG)
     #config = run.config
-    wandb_logger = WandbLogger(project="fashion_mnist_lit", config=CONFIG)
     dm = FashionMNISTDataModule(learning_rate=LEARNING_RATE)
     model = LitFashionMNIST(dm.num_classes, dm.learning_rate)
-    callbacks = [
-        L.ModelCheckpoint(
-            dirpath="./checkpoints",
-            every_n_train_steps=1,
-        ),
-    ]
+    callbacks = [ModelCheckpoint(dirpath="./checkpoints", every_n_train_steps=1)]
+    wandb_logger = WandbLogger(project="fashion_mnist_lit", config=CONFIG)
     #добавить в wandb "loss": loss
     trainer = L.Trainer(
         max_epochs=EPOCHS,
