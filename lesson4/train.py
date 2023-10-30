a822f3f02a023974aff4bbdd53bc91c1516ac16f
Valueva Anastasiya <valueva200211@gmail.com>
2023-10-31 01:53:31 +0300

    Пытаюсь добавить wandb

--- lesson4/train.py
+++ lesson4/train.py
@@ -23,11 +23,13 @@ def train(PATH):
     dm = FashionMNISTDataModule(learning_rate=LEARNING_RATE)
     config.learning_rate = LEARNING_RATE
     model = LitFashionMNIST(dm.num_classes, dm.learning_rate)
+    #добавить в wandb "loss": loss
     trainer = L.Trainer(
         max_epochs=EPOCHS,
         accelerator="auto",
         devices=1,
-        default_root_dir=PATH
+        default_root_dir=PATH,
+        logger=run
     )
     trainer.fit(model, dm)
     checkpoint_callback = L.ModelCheckpoint(dirpath=PATH, save_top_k=2, monitor="val_loss")
